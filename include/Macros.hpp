774df30fcd8b8e346f6b0c85a9a82a9753acd7c8
pederson <dpederson@utexas.edu>
2018-06-27 08:10:45 -0500

    (added) a fix to the macros

--- include/Macros.hpp
+++ include/Macros.hpp
@@ -31,18 +31,29 @@ namespace libra{
 // to every item passed in the variadic argument list. The argument "op"
 // is a macro that is expanded in between each application of "what"
 #define LIBRA_FOR_EACH_1(what, op, x, ...) what(x)
-#define LIBRA_FOR_EACH_2(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_1(what, LIBRA_DEFER(op), __VA_ARGS__)
-#define LIBRA_FOR_EACH_3(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_2(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_2(what, op, x, ...) what(x) op LIBRA_FOR_EACH_1(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_3(what, op, x, ...) what(x) op LIBRA_FOR_EACH_2(what, LIBRA_DEFER(op), __VA_ARGS__)
 #define LIBRA_FOR_EACH_4(what, op, x, ...) what(x) op LIBRA_FOR_EACH_3(what, LIBRA_DEFER(op), __VA_ARGS__)
-#define LIBRA_FOR_EACH_5(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_4(what, LIBRA_DEFER(op), __VA_ARGS__)
-#define LIBRA_FOR_EACH_6(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_5(what, LIBRA_DEFER(op), __VA_ARGS__)
-#define LIBRA_FOR_EACH_7(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_6(what, LIBRA_DEFER(op), __VA_ARGS__)
-#define LIBRA_FOR_EACH_8(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_7(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_5(what, op, x, ...) what(x) op LIBRA_FOR_EACH_4(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_6(what, op, x, ...) what(x) op LIBRA_FOR_EACH_5(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_7(what, op, x, ...) what(x) op LIBRA_FOR_EACH_6(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_8(what, op, x, ...) what(x) op LIBRA_FOR_EACH_7(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_9(what, op, x, ...) what(x) op LIBRA_FOR_EACH_8(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_10(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_9(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_11(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_10(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_12(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_11(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_13(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_12(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_14(what, op, x, ...) what(x) op LIBRA_FOR_EACH_13(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_15(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_14(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_16(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_15(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_17(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_16(what, LIBRA_DEFER(op), __VA_ARGS__)
+#define LIBRA_FOR_EACH_18(what, op, x, ...)	what(x) op LIBRA_FOR_EACH_17(what, LIBRA_DEFER(op), __VA_ARGS__)
+
 
 #define LIBRA_FOR_EACH_NARG(...) LIBRA_FOR_EACH_NARG_(__VA_ARGS__, LIBRA_FOR_EACH_RSEQ_N())
 #define LIBRA_FOR_EACH_NARG_(...) LIBRA_FOR_EACH_ARG_N(__VA_ARGS__) 
-#define LIBRA_FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, N, ...) N 
-#define LIBRA_FOR_EACH_RSEQ_N() 8, 7, 6, 5, 4, 3, 2, 1, 0
+#define LIBRA_FOR_EACH_ARG_N(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, N, ...) N 
+#define LIBRA_FOR_EACH_RSEQ_N() 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
 #define LIBRA_FOR_EACH_(N, what, op, x, ...) LIBRA_CONCATENATE(LIBRA_FOR_EACH_, N)(what, LIBRA_DEFER(op), x, __VA_ARGS__)
 #define LIBRA_FOR_EACH(what, op, x, ...) LIBRA_FOR_EACH_(LIBRA_FOR_EACH_NARG(x, __VA_ARGS__), what, LIBRA_DEFER(op), x, __VA_ARGS__)
